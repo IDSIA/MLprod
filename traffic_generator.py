e0bc0f20122b64da71c5bd254a9aa5a9fd3469cf
Claudio Bonesana <claudio.bonesana@supsi.ch>
2022-08-03 13:36:22 +0000

    Script traffic_generator now support multiple workers.

--- traffic_generator.py
+++ traffic_generator.py
@@ -1,7 +1,6 @@
 from time import sleep
-from dotenv import load_dotenv
-from api.requests import LocationData, UserData
 
+from api.requests import LocationData, UserData
 from datas import read_user_config, generate_user_data_from_config, UserLabeller
 
 import numpy as np
@@ -10,30 +9,26 @@ import argparse
 import logging
 import os
 
-# Environment variables are controlled through a .env file
-load_dotenv()
-
-
-if 'URL' in os.environ:
-    URL = os.environ.get('URL')
-else:
-    DOMAIN = os.environ.get('DOMAIN', 'localhost')
-    URL = f'http://mlpapi.{DOMAIN}'
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(name)s %(levelname)s %(message)s',
+)
 
 
 def setup_arguments():
     """Defines the available input parameters"""
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--seed', help='Set fixed seed', default=None, type=int)
+    parser.add_argument('--seed', help='Set fixed seed', default=42, type=int)
     parser.add_argument('-n', help='Number of requests to do, 0 means infinite', default=None, type=int)
     parser.add_argument('--no-sleep', help='If set, no sleeps are used', default=False, action='store_true')
     parser.add_argument('--config', help='User config file to use', default='./config/user.tsv', type=str)
+    parser.add_argument('-p', help='Number of parallel generators.', default=1, type=int)
 
     return parser.parse_args()
 
 
-def sleepy(time:int, flag:bool=True):
+def sleepy(thread: int, time: int, flag: bool=True):
     """Add some delay in the system.
     
     :param time:
@@ -43,15 +38,15 @@ def sleepy(time:int, flag:bool=True):
       If True (default value), the sleepy is executed, otherwise no 
     """
     if flag:
-        logging.info(f'Sleeping for {time}')
+        logging.info(f'{thread:02} Sleeping for {time*1000:.0f}ms')
         sleep(time)
 
 
-def inference_start(user: UserData) -> str:
+def inference_start(url: str, user: UserData) -> str:
     data = user.dict()
     data['time_arrival'] = str(data['time_arrival'])
     p_data = requests.post(
-        url=f'{URL}/inference/start', 
+        url=f'{url}/inference/start', 
         headers={
             'accept': 'application/json',
             'Content-Type': 'application/json',
@@ -62,22 +57,21 @@ def inference_start(user: UserData) -> str:
     return p_data.json()['task_id']
 
 
-def inference_status(task_id: str) -> bool:
+def inference_status(url: str, task_id: str) -> bool:
     g_status = requests.get(
-        url=f'{URL}/inference/status/{task_id}',
+        url=f'{url}/inference/status/{task_id}',
         headers={
             'accept': 'application/json',
         },
     )
 
     status = g_status.json()
-    logging.info(f'Request status: {status["status"]}')
-    return status['status'] == 'SUCCESS'
+    return status['status']
 
 
-def inference_results(task_id: str) -> dict:
+def inference_results(url: str, task_id: str) -> dict:
     g_results = requests.get(
-        url=f'{URL}/inference/results/{task_id}',
+        url=f'{url}/inference/results/{task_id}',
         headers={
             'accept': 'application/json',
         },
@@ -87,9 +81,9 @@ def inference_results(task_id: str) -> dict:
     return [LocationData(**d) for d in data]
 
 
-def make_choice(task_id: str, location_id: int) -> dict:
+def make_choice(url: str, task_id: str, location_id: int) -> dict:
     u_result = requests.put(
-        url=f'{URL}/inference/select/',
+        url=f'{url}/inference/select/',
         headers={
             'accept': 'application/json',
             'Content-Type': 'application/json',
@@ -103,21 +97,15 @@ def make_choice(task_id: str, location_id: int) -> dict:
     return u_result.json()
 
 
-if __name__ == '__main__':
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s %(name)s %(levelname)s %(message)s',
-    )
+def exec(args) -> None:
+    """"""
+    N, seed, config, url, thread = args
 
-    args = setup_arguments()
-
-    user_configs = read_user_config(args.config)
+    r = np.random.default_rng(seed=seed)
+    user_configs = read_user_config(config)
     ul = UserLabeller()
 
-    r = np.random.default_rng(args.seed)
-
-    n = 1 if args.n is None else args.n
-
+    n = 1 if N is None else N
     while n > 0:
         T1 = r.integers(2, 30) * .1
         T2 = r.integers(2, 30) * .1
@@ -126,24 +114,28 @@ if __name__ == '__main__':
         # choose next user ----------------------------------------
         user_config = r.choice(user_configs)
 
-        logging.info(f'User: {user_config["name"]}')
+        logging.info(f'{thread:02} User: {user_config["name"]}')
 
-        sleepy(T1)
+        sleepy(thread, T1)
 
         user = generate_user_data_from_config(r, user_config)
 
         # send new inference request ------------------------------
-        task_id = inference_start(user)
+        task_id = inference_start(url, user)
 
-        logging.info(f'Task id assigned: {task_id}')
+        logging.info(f'{thread:02} Task id assigned: {task_id}')
 
         # send task get request -----------------------------------
         done = False
         while not done:
-            sleepy(T2)
-            done = inference_status(task_id)
+            sleepy(thread, T2)
+            status = inference_status(url, task_id)
 
-        locations = inference_results(task_id)
+            logging.info(f'{thread:02} Request status: {status}')
+            done = status == 'SUCCESS'
+
+
+        locations = inference_results(url, task_id)
 
         # Make choice ---------------------------------------------
         labels = ul(r, user, locations)
@@ -151,12 +143,40 @@ if __name__ == '__main__':
 
         location_id = int(r.choice(location_ids[labels == 1]))
         
-        logging.info(f'Choosen location with id {location_id}')
+        logging.info(f'{thread:02} Choosen location with id {location_id}')
 
         # Register choice -----------------------------------------
-        sleepy(T3)
-        make_choice(task_id, location_id)
+        sleepy(thread, T3)
+        make_choice(url, task_id, location_id)
 
         # next request --------------------------------------------
-        if args.n is not None:
+        if N is not None:
             n -= 1
+
+
+if __name__ == '__main__':
+    from dotenv import load_dotenv
+    from multiprocessing import Pool, set_start_method
+    set_start_method('spawn')
+
+    # Environment variables are controlled through a .env file
+    load_dotenv()
+
+    if 'URL' in os.environ:
+        URL = os.environ.get('URL')
+    else:
+        DOMAIN = os.environ.get('DOMAIN', 'localhost')
+        URL = f'http://mlpapi.{DOMAIN}'
+
+    args = setup_arguments()
+
+    r = np.random.default_rng(args.seed)
+
+    workers = min(os.cpu_count(), args.p)
+
+    params = [(args.n, args.seed+i, args.config, URL, i+1) for i in range(workers)]
+
+    logging.info(f'Starting traffic generation with {workers} worker(s)')
+
+    with Pool(processes=workers) as pool:
+        pool.map(exec, params)
